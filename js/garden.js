1b79d8ed581ffb130eb0a2c73b365746c4697f26
QtBag <2405501986@qq.com>
2018-11-29 06:04:33 -0800

    2018/11/29

--- js/garden.js
+++ js/garden.js
@@ -43,8 +43,6 @@ function Petal(stretchA, stretchB, startAngle, angle, growFactor, bloom) {
     this.growFactor = growFactor;
     this.r = 1;
     this.isfinished = false;
-    //this.tanAngleA = Garden.random(-Garden.degrad(Garden.options.tanAngle), Garden.degrad(Garden.options.tanAngle));
-    //this.tanAngleB = Garden.random(-Garden.degrad(Garden.options.tanAngle), Garden.degrad(Garden.options.tanAngle));
 }
 
 Petal.prototype = {
@@ -53,8 +51,8 @@ Petal.prototype = {
         var v1, v2, v3, v4;
         v1 = new Vector(0, this.r).rotate(Garden.degrad(this.startAngle));
         v2 = v1.clone().rotate(Garden.degrad(this.angle));
-        v3 = v1.clone().mult(this.stretchA); //.rotate(this.tanAngleA);
-        v4 = v2.clone().mult(this.stretchB); //.rotate(this.tanAngleB);
+        v3 = v1.clone().mult(this.stretchA);
+        v4 = v2.clone().mult(this.stretchB);
         ctx.strokeStyle = this.bloom.c;
         ctx.beginPath();
         ctx.moveTo(v1.x, v1.y);
@@ -63,7 +61,7 @@ Petal.prototype = {
     },
     render: function () {
         if (this.r <= this.bloom.r) {
-            this.r += this.growFactor; // / 10;
+            this.r += this.growFactor;
             this.draw();
         } else {
             this.isfinished = true;
